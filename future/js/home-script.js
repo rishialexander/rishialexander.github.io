070761a9bd6a99ea86919a3127fa08c7bba6ffe5
Rishi <rishialexander@gmail.com>
2016-06-03 12:01:54 -0700

    2nd major version with cards working and proper videos

--- future/js/home-script.js
+++ future/js/home-script.js
@@ -1,20 +1,76 @@
 $( document ).ready(function() {
 
+	var vid1 = false;
+	var vid2 = false;
+	var vid3 = false;
+	var vid4 = false;
+	var vid5 = false;
+
+
 	$(window).scroll(function(){
-		if ($('#text-1').visible() && ($('#phone-inset video').attr('src') != 'assets/videos/hero.mp4')) {
-			$('#phone-inset video').attr('src', 'assets/videos/hero.mp4');
+
+		if ($("#hero-splash")[0].getBoundingClientRect().bottom <= 0) {
+			$('#app-right').css('position', 'fixed');
+			$('#app-right').css('top', '0');
+		} else {
+			$('#app-right').css('position', 'absolute');
+			$('#app-right').css('top', '100vh');
+		}
+
+		if ($('#text-1').visible() && ( !vid1 )) {
+			$('#phone-block').fadeTo(75, 1, function(){
+				$('#phone-inset video').attr('src', 'assets/videos/hero.mp4');
+				$('#phone-block').fadeTo(75, 0);
+			});
+			vid1 = true;
+			vid2 = false;
+			vid3 = false;
+			vid4 = false;
+			vid5 = false;
 		} 
-		if ($('#text-2').visible() && ($('#phone-inset video').attr('src') != 'assets/videos/budtender.mp4')) {
-			$('#phone-inset video').attr('src', 'assets/videos/budtender.mp4');
+		if ($('#text-2').visible() && ( !vid2 )) {
+			$('#phone-block').fadeTo(75, 1, function(){
+				$('#phone-inset video').attr('src', 'assets/videos/budtender.mp4');
+				$('#phone-block').fadeTo(75, 0);
+			});
+			vid1 = false;
+			vid2 = true;
+			vid3 = false;
+			vid4 = false;
+			vid5 = false;
 		} 
-		if ($('#text-3').visible() && ($('#phone-inset video').attr('src') != 'assets/videos/history.mp4')) {
-			$('#phone-inset video').attr('src', 'assets/videos/history.mp4');
+		if ($('#text-3').visible() && ( !vid3 )) {
+			$('#phone-block').fadeTo(75, 1, function(){	
+				$('#phone-inset video').attr('src', 'assets/videos/history.mp4');
+				$('#phone-block').fadeTo(75, 0);
+			});
+			vid1 = false;
+			vid2 = false;
+			vid3 = true;
+			vid4 = false;
+			vid5 = false;
 		} 
-		if ($('#text-4').visible() && ($('#phone-inset video').attr('src') != 'assets/videos/grower.mp4')) {
-			$('#phone-inset video').attr('src', 'assets/videos/grower.mp4');
+		if ($('#text-4').visible() && ( !vid4 )) {
+			$('#phone-block').fadeTo(75, 1, function(){	
+				$('#phone-inset video').attr('src', 'assets/videos/grower.mp4');
+				$('#phone-block').fadeTo(75, 0);
+			});
+			vid1 = false;
+			vid2 = false;
+			vid3 = false;
+			vid4 = true;
+			vid5 = false;
 		} 
-		if ($('#text-5').visible() && ($('#phone-inset video').attr('src') != 'assets/videos/beta.mp4')) {
-			$('#phone-inset video').attr('src', 'assets/videos/beta.mp4');
+		if ($('#text-5').visible() && ( !vid5 )) {
+			$('#phone-block').fadeTo(75, 1, function(){	
+				$('#phone-inset video').attr('src', 'assets/videos/beta.mp4');
+				$('#phone-block').fadeTo(75, 0);
+			});
+			vid1 = false;
+			vid2 = false;
+			vid3 = false;
+			vid4 = false;
+			vid5 = true;
 		}
 		$('#phone-inset').css('background-size', 'cover');   
 	});
--- future/js/inspiration-script.js
+++ future/js/inspiration-script.js
@@ -1,41 +1,42 @@
-$( document ).ready(function() {
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+$(window).load(function() {
+
+	var numCards = $('#cards').children().length;
+	var totalOffset = 0;
+	for (var i = 0; i < numCards; i++) {
+		var offset = $('#cards').children().eq(i)[0].getBoundingClientRect().top + $('#cards').children().eq(i).outerHeight();
+		totalOffset += offset;
+		$('#cards').children().eq(i).attr('data-offset', totalOffset);
+	}
+
+	$(window).resize(function() {
+		$(window).scrollTop(0);
+		totalOffset = 0;
+		for (var i = 0; i < numCards; i++) {
+			var offset = $('#cards').children().eq(i)[0].getBoundingClientRect().top + $('#cards').children().eq(i).outerHeight();
+			totalOffset += offset;
+			$('#cards').children().eq(i).attr('data-offset', totalOffset);
+		}		
+	});
+
+
 
 	$(window).scroll(function(){
-		if ($(document).scrollTop() >= 0) {
-			$('#card-1').css('position', 'relative');
-			$('#card-2').css('position', 'fixed');
-			$('#card-3').css('position', 'fixed');
-			$('#card-4').css('position', 'fixed');
-			$('#card-5').css('position', 'fixed');
-		}
-		if ($(document).scrollTop() > 700) {
-			$('#card-1').css('position', 'relative');
-			$('#card-2').css('position', 'relative');
-			$('#card-3').css('position', 'fixed');
-			$('#card-4').css('position', 'fixed');
-			$('#card-5').css('position', 'fixed');
-		}
-		if ($(document).scrollTop() > 1400) {
-			$('#card-1').css('position', 'relative');
-			$('#card-2').css('position', 'relative');
-			$('#card-3').css('position', 'relative');
-			$('#card-4').css('position', 'fixed');
-			$('#card-5').css('position', 'fixed');
-		}
-		if ($(document).scrollTop() > 2100) {
-			$('#card-1').css('position', 'relative');
-			$('#card-2').css('position', 'relative');
-			$('#card-3').css('position', 'relative');
-			$('#card-4').css('position', 'relative');
-			$('#card-5').css('position', 'fixed');
-		}
-		if ($(document).scrollTop() > 2800) {
-			$('#card-1').css('position', 'relative');
-			$('#card-2').css('position', 'relative');
-			$('#card-3').css('position', 'relative');
-			$('#card-4').css('position', 'relative');
-			$('#card-5').css('position', 'relative');
+
+		for (var i = 1; i < numCards; i++) {
+			var thisOffset = $('#cards').children().eq(i - 1).attr('data-offset');
+			if ($(document).scrollTop() > thisOffset) {
+				$('#cards').children().eq(i).css('position', 'relative');
+			} else {
+				$('#cards').children().eq(i).css('position', 'fixed');
+			}
 		}
 
 	});
 });
+
+
+
